85899a1e1022c7675bbb0671adcaa34f79927f0b
Lion Zhao <lionzjg00@hotmail.com>
2022-01-24 16:37:06 +0800

    math operators

--- hua.py
+++ hua.py
@@ -6,16 +6,41 @@ from lib.math import star
 from lib.math import triangle
 
 x = 100
-print(x)
+print("x=",x, "...",type(x))
 
 x=True
-print(x)
+print("x=",x, "....",type(x))
+
+x=5+6j
+print("x=",x, "....",type(x))
+y=3+2j
+print("x+y=",x+y, "....",type(x+y))
+
+x=5
+y = float(x)+1.0
+print("x=",x, "....",type(x), "y = float(x)+1.0=",y, "....",type(y))
+
+x=17
+y=5
+print("x=",x," y=",y)
+print("x+y= ",x+y)
+print("x-y= ",x-y)
+print("x*y= ", x*y)
+print("x/6= ", x/y)
+print("x%y= ", x%y)
+print("x//y= ",x//y)
+
+x=5
+y=2
+print("\n x=",x," y=",y)
+print("x**y=",x**y)
+
 
 x = "I love python!"
 print(x)
 
-star(3)
+# star(3)
 
-result = triangle(4)
-print("Function triangle result is ", result)
+# result = triangle(4)
+# print("Function triangle result is ", result)
 
