538dafd9bb5787154b0e16e52c1444bce9c58db6
Lion Zhao <lionzjg00@hotmail.com>
2022-01-25 12:33:06 +0800

    first learning of python

--- hua.py
+++ hua.py
@@ -2,42 +2,71 @@
 # -*- coding: utf-8 -*-
 # 用于注释的表示符号
 
+from tkinter.messagebox import showinfo
 from lib.math import star
 from lib.math import triangle
-
-x = 100
-print("x=",x, "...",type(x))
-
-x=True
-print("x=",x, "....",type(x))
-
-x=5+6j
-print("x=",x, "....",type(x))
-y=3+2j
-print("x+y=",x+y, "....",type(x+y))
-
-x=5
-y = float(x)+1.0
-print("x=",x, "....",type(x), "y = float(x)+1.0=",y, "....",type(y))
-
-x=17
-y=5
-print("x=",x," y=",y)
-print("x+y= ",x+y)
-print("x-y= ",x-y)
-print("x*y= ", x*y)
-print("x/6= ", x/y)
-print("x%y= ", x%y)
-print("x//y= ",x//y)
-
-x=5
-y=2
-print("\n x=",x," y=",y)
-print("x**y=",x**y)
+from lib.logy import compare
+from lib.math import digital
+from lib.logy import logy_op
+from lib.math import di_op
+from lib.math import tri
+from lib.array import Array
+from lib.array import fun_set
+from lib.array import tup
+from lib.math import f1,f2
+from lib.math import calc
+from lib.logy import filtred
+from lib.logy import mapping
+from lib.obje import showdog, showdog_property
+from lib.parameters import addmore, showmore
 
 
 x = "I love python!"
 print(x)
+print("------------------------")
+
+# result = addmore(1,45,19,18,25,93)
+# print("the result is {}".format(result))
+showmore(na = "tony",ag=18, sex="M")
+
+
+# PI=3.1415926
+# print('%-10.5f' %PI) #左对齐，还是10个字符，但空格显示在右边。
+
+# showdog_property()
+# showdog()
+
+# dataset = {2, 8,15,30,45,7,9}
+# y = filtred(dataset)
+# print("original data set is :", dataset)
+# print("filtered data set is :", y)
+# z=mapping(dataset)
+# print("mapping data set is :",z)
+
+# print("a+b = ", f1(25,5))
+# print("a-b=", f2(25,3))
+
+# f3=calc("+")
+# f4=calc("4")
+
+# print("lambda: a+b", f3(25,5))
+# print("lambda: a-b",f4(25,3))
+# print("success")
+
+
+# tup()
+
+# Array(5)
+# fun_set()
+# tri(2)
+# compare(17,5)
+# logy_op(5,2,3,4)
+# a = int(input("please input 2 numbers for calculation.\n"))
+# b = int(input())
+# di_op(a,b)
+
+
+# digital()
 
 # star(3)
 
